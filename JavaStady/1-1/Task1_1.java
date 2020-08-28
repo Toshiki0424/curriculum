cc99628ad36e79140dfda25ced36cff650331752
Toshiki0424 <69826314+Toshiki0424@users.noreply.github.com>
2020-08-28 23:03:09 +0900

    課題提出
    Task1_1

---
+++ JavaStady/1-1/Task1_1.java
@@ -0,0 +1,24 @@
+public class Task1_1{
+
+    public static void main(String[] args){
+
+        int hogeInt;
+        hogeInt = 50;
+   
+        String hogeString;
+        hogeString = "テスト";
+        
+        double hogeDouble = 0.2;
+         // ③下記の処理について、何をしているのかコメントを記入してください
+        // [hogedoubleという名前の変数を宣言、値を「0.2」で初期化]
+        
+        
+        hogeInt = 100;
+        hogeString = "合格";
+        System.out.println(hogeInt);
+        System.out.println(hogeString);
+        // ⑦下記の処理について、何をしているのかコメントを記入してください
+        // hogeStringの値を表示
+        System.out.println(hogeDouble);
+    }
+}
