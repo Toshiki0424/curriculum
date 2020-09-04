2b2515a38f20d25dc1a6986f53404c5d0f43da09
Toshiki0424 <69826314+Toshiki0424@users.noreply.github.com>
2020-09-04 13:26:29 +0900

    課題再再提出
    Task1_4

--- JavaStady/1-4/Task1_4.java
+++ JavaStady/1-4/Task1_4.java
@@ -1,3 +1,4 @@
+
 /**
  * Task1-4: 課題内容
  *
@@ -9,8 +10,8 @@ public class Task1_4 {
 
     // 定数（条件）
     // ① 名前とパスワードの定数を定義してください。
-    String name;
-    String pass;
+    final String name_final = "名前";
+    final String pass_final = "パスワード";
 
 
     // 補足:
@@ -31,17 +32,17 @@ public class Task1_4 {
         String name = "alice";
         String pass = "pass";
         // ② 名前が「 alice 」、パスワードが「 pass 」であった場合、「 ログイン成功です 」と出力してください。
-        if (name == "alice" && pass == "pass"){
+        if (name.equals("alice") && pass.equals( "pass")){
             System.out.println(CONST_MSG_SUCCESS);
 
 
         // ③ 名前のみ正しい場合、「 パスワードに誤りがあります。 」と出力してください。
-         } else if (name == "alice"){
+         } else if (name.equals("alice")){
             System.out.println(CONST_MSG_ERROR_PASS);
         
 
         // ④ パスワードのみ正しい場合、「 名前に誤りがあります。 」と出力してください。
-          } else if (pass == "pass"){
+          } else if (pass.equals("pass")){
             System.out.println(CONST_MSG_ERROR_NAME);
         
         
